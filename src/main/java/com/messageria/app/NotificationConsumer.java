3b7ae0de4148b1ea7abdc88828722c742e9355c9
Arthurnucada <131211101+Arthurnucada@users.noreply.github.com>
2025-06-23 03:23:40 -0300

    Initial Commit

---
+++ src/main/java/com/messageria/app/NotificationConsumer.java
@@ -0,0 +1,16 @@
+
+package com.messageria.app;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationConsumer {
+    @KafkaListener(topics = "inventory-events", groupId = "notification-group")
+    public void notificaCliente(String message) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        InventoryEvent event = mapper.readValue(message, InventoryEvent.class);
+        System.out.printf("[Aviso] Pedido %s status: %s\n", event.getOrderId(), event.getStatus());
+    }
+}
